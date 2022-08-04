75958ebc2c7a0007e63ea3eb1b8de0dd7d7f4260
PascanMihai <pascan.mihai@mail.ru>
2022-08-04 20:13:40 +0300

    Created new controller fot authentication, added required nugets, created models folder with loginmodel

--- Api/Controllers/AuthController.cs
+++ Api/Controllers/AuthController.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using Api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("rtyui")]
+        public async Task<string> Login(LoginModel login)
+        {
+            return login.UserName + login.Password;
+        }
+    }
+}
--- Api/Program.cs
+++ Api/Program.cs
@@ -1,31 +1,25 @@
-using Microsoft.AspNetCore.Builder;
-
 var builder = WebApplication.CreateBuilder(args);
 
-//builder.Services.AddDbContext<>();
+// Add services to the container.
 
-builder.Services.AddSwaggerGen
-
-builder.Services.Controllers().AddNewtonsoftJson{
-
-}
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-app.UseStaticFiles();
 
-app.UseRouting();
+app.UseAuthorization();
 
-//app.UseAuthorization();
-
+app.MapControllers();
 
 app.Run();

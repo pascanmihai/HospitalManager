72bd8c0fbae45efcccbe20115fa5246deea427ad
PascanMihai <pascan.mihai@mail.ru>
2022-08-04 18:49:14 +0300

    Initial commit create project

---
+++ HospitalManager/Controllers/AuthController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HospitalManager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+    }
+}
